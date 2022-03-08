32e5a9b484be654868ae682a04927e73fa1dc906
Milton Palmeira <milton.santana@2rpnet.com>
2022-03-08 14:14:14 -0300

    auth

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express'
+import { User } from '../models/User';
+export const Auth = {
+    private: async (req: Request, res: Response, next: NextFunction) => {
+        let success = false;
+
+        // Fazer verificação de auth
+        if (req.headers.authorization) {
+            let hash: string = req.headers.authorization.substring(6);
+            let decoded: string = Buffer.from(hash, 'base64').toString();
+            let data: string[] = decoded.split(':');
+            if (data.length === 2) {
+                let hasUser = await User.findOne({
+                    where: {
+                        email: data[0],
+                        password: data[1]
+                    }
+                });
+                if (hasUser)
+                    success = true;
+            }
+        }
+
+        if (success) {
+            next();
+        }
+        else {
+            res.status(403); // Not authorized
+            res.json({error: 'Não autorizado'});
+        }
+    }    
+}
